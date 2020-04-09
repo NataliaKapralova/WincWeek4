74e3b997b735dd46b151404d396825d504a753a9
Natalia Kapralova <n.kapralovaa@live.nl>
2020-04-09 12:01:56 +0200

    new files added

---
+++ W4D4-2/functions.js
@@ -0,0 +1,25 @@
+const addOne = function(numbers) {
+	
+}
+
+module.exports = addOne;
+
+
+const getWordLengths = function(someWords) {
+    const myArray = [31, 57, 12, 5];
+
+    const unchanged = [31, 57, 12, 5];
+    const expected = [32, 58, 13, 6];
+    const output = addOne(myArray);
+  
+};
+
+module.exports = getWordLengths;
+
+const findNeedle = function(words) {
+    if(words === 3) { 
+        return expected
+    }
+  };
+  
+  module.exports = findNeedle;
